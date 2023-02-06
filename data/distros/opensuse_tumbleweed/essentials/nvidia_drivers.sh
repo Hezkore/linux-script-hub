d8588de264e1143d53052a6b1341f8d15d7b97d0
Rob C <hezkore@gmail.com>
2023-02-06 13:59:52 +0100

    added nvidia-utils-G06

--- data/distros/opensuse_tumbleweed/essentials/nvidia_drivers.sh
+++ data/distros/opensuse_tumbleweed/essentials/nvidia_drivers.sh
@@ -4,7 +4,7 @@
 
 # Name: Install proprietary NVIDIA drivers
 # Description: Adds the NVIDIA repository and installs the NVIDIA drivers, specifically the 64bit G06 version.
-# Installs support for GPGPU computing, OpenGL libraries, and the video driver.
+# Installs support for GPGPU computing, OpenGL libraries, driver tools, and the video driver.
 # Version G06 is compatible with all cards GT700 and up.
 # Note that this script assumes that the multiversion feature for kernel packages is enabled.
 
@@ -12,5 +12,6 @@ zypper --non-interactive addrepo --refresh https://download.nvidia.com/opensuse/
 zypper --non-interactive install nvidia-compute-G06
 zypper --non-interactive install nvidia-gl-G06
 zypper --non-interactive install nvidia-video-G06
+zypper --non-interactive install nvidia-utils-G06
 
 restart_required
